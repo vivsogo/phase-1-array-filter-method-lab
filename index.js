770908fdf3254fa66b3229f46cfbc75210808dc4
Vivian Gomez <viviangomez0@gmail.com>
2021-11-11 21:52:07 -0500

    commit tree 7eff1fb40d5cbf43d22ce79e89188e4f5a514111

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(drivers, string) {
+    return drivers.filter(function (driver) { return driver.toLowerCase() === string.toLowerCase() })
+  }
+ 
+  function fuzzyMatch(drivers, string) {
+    return drivers.filter(function (driver) { return driver.toLowerCase().substring(0, string.length) === attribute.toLowerCase() })
+  }
+ 
+  function matchName(drivers, string) {
+    return drivers.filter(function (driver) { return driver.name === argument })
+  }
